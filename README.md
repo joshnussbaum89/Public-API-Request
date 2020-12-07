20adce61574310cc22a38794dcd5145fe788aacf
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-12-07 12:18:20 -0500

    edit README and add event listener for prev and next modal buttons

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Public API Request
 
 1. Returns a list of random users from the Random User Generator API
-2. Displays a modal to show their information
+2. Search for users with search bar at the top of the page
+3. Displays a modal to show their information
+4. Toggle between users with "prev" and "next" buttons
+5. Close modal with "X" button on the top right of the modal
--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-// Global Variables
 const url = 'https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US';
 const gallery = document.querySelector('#gallery');
 const body = document.querySelector('body');
 let employeeData = [];
 let index = 0;
 
-// Retrieve data from Random User Generator API
+
+/* ======================================================================= */
+/*                 Fetch Data from Random User Generator API
+/* ======================================================================= */
+
+
 fetch(url)
     .then(res => res.json())
     .then(res => res.results)
     .then(displayEmployeeCard)
     .catch(error => console.log('Looks like there was a problem', error))
+
+
+/* ======================================================================= */
+/*                            Functions
+/* ======================================================================= */
+
 
 // Display employee cards on page load
 function displayEmployeeCard(employee) {
@@ -20,7 +30,7 @@ function displayEmployeeCard(employee) {
         gallery.insertAdjacentHTML('beforeend', `
             <div class="card" data-index="${index}">
                 <div class="card-img-container">
-                    <img class="card-img" src="${user.picture.thumbnail}" alt="profile picture">
+                    <img class="card-img" src="${user.picture.large}" alt="profile picture">
                 </div>
                 <div class="card-info-container">
                     <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
@@ -36,14 +46,14 @@ function displayEmployeeCard(employee) {
 // Populates HTML for employee modal
 function openEmployeeModal(index) {
     const employeeIndex = employeeData[index];
-    const date = new Date(employeeData[index].dob.date);
+    const date = new Date(employeeIndex.dob.date);
 
     const modalHTML = `
         <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src="${employeeIndex.picture.medium}" alt="profile picture">
+                        <img class="modal-img" src="${employeeIndex.picture.large}" alt="profile picture">
                         <h3 id="name" class="modal-name cap">${employeeIndex.name.first} ${employeeIndex.name.last}</h3>
                         <p class="modal-text">${employeeIndex.email}</p>
                         <p class="modal-text cap">${employeeIndex.location.city}</p>
@@ -63,16 +73,69 @@ function openEmployeeModal(index) {
     `
 
     body.insertAdjacentHTML('beforeend', modalHTML);
+
 }
+
+// Close modal when user clicks 'X'
+function closeModal(modal) {
+    modal.remove();
+}
+
+function searchEmployees() {
+    // Search for employees
+}
+
+
+/* ======================================================================= */
+/*                            Event Listeners
+/* ======================================================================= */
+
 
 // Listens for user clicks on employee cards, opens modal
 gallery.addEventListener('click', e => {
-    // Make sure click event is not the gallery
     if (e.target !== gallery) {
         const card = e.target.closest('.card');
         const indexNum = card.getAttribute('data-index');
         index = indexNum;
         openEmployeeModal(index);
     }
-
 });
+
+// Listen for user interaction when modal is open
+// If user clicks the 'X', call closeModal
+// If user clicks 'Prev' or 'Next' buttons, change modals accordingly
+body.addEventListener('click', (e) => {
+    const modal = document.querySelector('.modal-container');
+
+    if (modal) {
+        const closeBtn = document.querySelector('.modal-close-btn');
+        const modalBtnContainer = document.querySelector('.modal-btn-container');
+        const modalPrev = document.querySelector('.modal-prev');
+        const modalNext = document.querySelector('.modal-next');
+
+        closeBtn.addEventListener('click', (event) => {
+            const closeBtnStrong = document.querySelector('.modal-close-btn strong');
+            const eventTarget = event.target;
+
+            if (eventTarget === closeBtn || eventTarget === closeBtnStrong) {
+                closeModal(modal);
+            }
+        })
+
+        modalBtnContainer.addEventListener('click', (event) => {
+            closeModal(modal);
+            const closeBtnStrong = document.querySelector('.modal-close-btn strong');
+            const eventTarget = event.target;
+
+            if (event.target === modalPrev && index > 0) {
+                index--;
+            } else if (event.target === modalNext && index < 11) {
+                index++;
+            } else if (eventTarget === closeBtn || eventTarget === closeBtnStrong) {
+                closeModal(modal);
+            }
+
+            openEmployeeModal(index);
+        });
+    }
+})
